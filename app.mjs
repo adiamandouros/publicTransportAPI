9f8c708197757d9670e6dc2e46892e16d9d16ba1
adiamandouros <adiamandouros@gmail.com>
2025-06-27 22:28:40 +0300

    route fix

--- app.mjs
+++ app.mjs
@@ -7,22 +7,21 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.use("/", router)
+app.use("/api/publicTransport", router)
 
 // οτιδήποτε άλλο θα ανακατευθύνεται στο "/"
 app.use((req, res) => {
     res.redirect('/')
 });
 
-// app.use((err, req, res, next) => {
-//     console.log(err.stack)
-//     res.status(500).json({
-//         status: "NOT OK",
-//         message: err.message
-//     });
-// });
-
-app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.use((err, req, res, next) => {
+    console.log(err.stack)
+    res.status(500).json({
+        status: "NOT OK",
+        message: err.message
+    });
+    next();
+});
 
 app.listen(PORT, () => {
   console.log("Server Listening on PORT:", PORT);
--- routes.mjs
+++ routes.mjs
@@ -3,14 +3,14 @@ import { getLocalStops, getStopArrivals, getRouteName, getStopRoutes, getLocalIn
 
 export const router = express.Router();
 
-router.get("", (request, response)=>{
-    response.status(200).json({
-        status: "OK",
-        message: "Welcome to the OASA API Translator",
-        dir: __dirname,
-        file: __filename
-    });
-});
+// router.get("", (request, response)=>{
+//     response.status(200).json({
+//         status: "OK",
+//         message: "Welcome to the OASA API Translator",
+//         dir: __dirname,
+//         file: __filename
+//     });
+// });
 
 router.get("/", (request, response)=>{
     response.status(200).json({
